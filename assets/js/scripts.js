b50f4bc4148b3a6cc60857394117c54281f4e956
Rémy <lymih@hotmail.com>
2025-06-18 16:06:12 +0200

    update: cleanup script file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,35 +13,5 @@ const init = () => {
 			popup();
 		}
 	}
-
-	if($('form').length){
-		contactForm();
-	}
-
-	if($('.home').length) {
-		const swiperHome = new Swiper('#home-swiper', {
-			direction: 'horizontal',
-			autoplay: {
-				delay: 5000,
-			}
-		})
-	}
-
-	if($('#swiper-realisatie').length) {
-		const swiperRealisatieThumbnails = new Swiper('#swiper-realisatie-thumbnails', {
-			direction: 'horizontal',
-			spaceBetween: 30,
-			slidesPerView: 3,
-			freeMode: true,
-			watchSlidesProgress: true,
-		})
-
-		const swiperRealisatie = new Swiper('#swiper-realisatie', {
-			direction: 'horizontal',
-			thumbs: {
-				swiper: swiperRealisatieThumbnails
-			}
-		})
-	}
 }
 

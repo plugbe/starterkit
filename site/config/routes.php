69bf6509fa0bab553c571c7247f15e855c2860ac
Rémy <lymih@hotmail.com>
2025-11-14 15:11:08 +0100

    Experimental: route for status page

--- site/config/routes.php
+++ site/config/routes.php
@@ -5,5 +5,33 @@ return [
         'pattern'   => 'contact',
         'method'    => 'POST',
         'action'    => $contactForm
-    ]
+    ],
+    [
+        'pattern' => 'status',
+        'method'  => 'GET',
+        'action'  => function () {
+
+            $secret = get('secret');
+            $allowed = 'SECRET';
+
+            if ($secret !== $allowed) {
+                return [
+                    'status'  => 'error',
+                    'message' => 'Forbidden'
+                ];
+            }
+
+            // Add proper CORS headers
+            kirby()->response()->header('Access-Control-Allow-Origin', 'https://hub.plugdev.be');
+            kirby()->response()->header('Access-Control-Allow-Methods', 'GET');
+            kirby()->response()->header('Content-Type', 'application/json');
+
+            return [
+                'php'         => phpversion(),
+                'kirby'       => kirby()->version(),
+                'debug'       => option('debug'),
+                'url'         => url()
+            ];
+        }
+    ],
 ];

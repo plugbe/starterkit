1d3b39d4d449b3cae455733f6928f863173def85
Rémy <lymih@hotmail.com>
2025-11-14 10:05:08 +0100

    Cleanup code for general use

--- assets/js/components/navigation.js
+++ assets/js/components/navigation.js
@@ -1,25 +1,107 @@
 export const menuToggle = () => {
     const container = document.querySelector('.header');
-    const toggle = container.querySelector('.toggle__wrapper');
-    const menu = document.querySelector('.mobile-navigation');
+    const toggle = container.querySelector('.hamburger');
+    const menu = document.querySelector('.fullmenu');
+    const closeToggle = menu.querySelector('.hamburger');
     const menuLinks = menu.querySelectorAll('li a');
     let menuVisible = false;
 
     toggle.addEventListener('click', () => {
         if(menuVisible == false) {
-            menu.classList.add('visible');
+            menu.classList.add('open');
             menuVisible = true;
-        } else {
-            menu.classList.remove('visible');
+        }
+    });
+
+    closeToggle.addEventListener('click', () => {
+        if(menuVisible == true) {
+            menu.classList.remove('open');
             menuVisible = false;
         }
     });
 
-    menuLinks.forEach((links) => {
-        links.addEventListener('click', (e) => {
-            e.preventDefault();
-            menu.classList.remove('visible');
-            menuVisible = false;
-        })
+    // menuLinks.forEach((links) => {
+    //     links.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         menu.classList.remove('visible');
+    //         menuVisible = false;
+    //     })
+    // })
+}
+
+export const scrollCheck = () => {
+    const nav = document.querySelector('.header');
+    const secondaryNav = document.querySelector('.header-secondary');
+    const secondaryNavContent = document.querySelector('.header-secondary-content');
+    const scrollThreshold = 10; // Adjust this threshold as needed
+
+    let previousScrollPosition = window.scrollY;
+
+	window.addEventListener('scroll', () => {
+        let currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+        // Detect when the user scrolls upwards
+        if (currentScroll > previousScrollPosition && currentScroll > 60) {
+            // nav.classList.add('scrolling');
+            // nav.classList.remove('visible');
+
+            // gsap.to(nav, {
+            //     y: 0,
+            //     autoAlpha: 1,
+            //     position: 'fixed',
+            //     // ease: "expo.inOut",
+            //     duration: 0.4,
+            //     paddingTop: "1.375rem",
+            //     paddingBottom: "1.000rem",
+            //     backgroundColor: "#EEEFED",
+            // })
+
+            nav.classList.add('minimize');
+            secondaryNav.classList.add('minimize');
+            secondaryNavContent.classList.add('minimize');
+        } else if (currentScroll < previousScrollPosition && previousScrollPosition - currentScroll > scrollThreshold) {
+            // gsap.to(nav, {
+            //     y: 0,
+            //     autoAlpha: 1,
+            //     position: 'fixed',
+            //     // ease: "expo.inOut",
+            //     duration: 0.4,
+            //     paddingTop: "1.375rem",
+            //     paddingBottom: "1.000rem",
+            //     backgroundColor: "#EEEFED",
+            // })
+        }
+
+        if (currentScroll < 80) {
+            nav.classList.remove('minimize');
+            secondaryNav.classList.remove('minimize');
+            secondaryNavContent.classList.remove('minimize');
+
+            if(window.innerWidth < 650) {
+                // gsap.to(nav, {
+                //     y: 0,
+                //     position: 'fixed',
+                //     autoAlpha: 1,
+                //     paddingTop: "1.5625rem",
+                //     paddingBottom: "1.5625rem",
+                //     backgroundColor: "#EEEFED",
+                //     // ease: "expo.inOut",
+                //     duration: 0.4,
+                // })
+            } else {
+                // gsap.to(nav, {
+                //     y: 0,
+                //     position: 'fixed',
+                //     autoAlpha: 1,
+                //     paddingTop: "4.375rem",
+                //     paddingBottom: "4.375rem",
+                //     backgroundColor: "transparent",
+                //     // ease: "expo.inOut",
+                //     duration: 0.4,
+                // })
+            }
+        }
+
+        previousScrollPosition = currentScroll <= 0 ? 0 : currentScroll;
     })
 }
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,26 @@
 import { fadeOut, fadeIn } from './components/transitions';
-import { contactForm } from './components/forms';
+import { form } from './components/forms';
 import { popup } from './components/popup';
+import { menuToggle, scrollCheck } from './components/navigation';
 
 
 const init = () => {
+	//Indien je popups gebruikt met fancybox hier fancybox init
+	//En data-fancybox gebruiken in template
+	// Fancybox.bind("[data-fancybox]", {
+	// 	zoomEffect: false,
+	// 	Carousel: {
+	// 		gestures: false,
+	// 		Zoomable: {
+	// 			Panzoom: {
+	// 				startPos: {
+	// 					scale: "full",
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// });
+
 	const popupWindow = document.querySelector('#hiddenmodal');
 
 	if(popupWindow) {
@@ -14,6 +31,47 @@ const init = () => {
 			popup(cookieKey);
 		}
 	}
+
+
+	//Een formulier koppelen met de ID
+	//form("#contact");
+	//form("#newsletter");
+
+	//Animaties hier init en className gebruiken in templates
+	//fadeOut(".fadeout");
+	//fadeIn(".fadein");
+
+
+	//Carousel met swiper kan als volgt:
+	// const sliderNaam = document.querySelector('#element');
+	// if(sliderNaam) {
+	// 	const swiper = new Swiper('#element', {
+	// 		direction: 'horizontal',
+	// 		loop: true,
+	// 		speed: 800,
+	// 		autoplay: {
+	// 			delay: 3000,
+	// 		}
+	// 	});
+	// }
+
+	//Indien een blok is dat meermaals op pagina voorkomt
+	// const imageBlock = document.querySelectorAll('.b-image');
+	// if(imageBlock.length > 0) {
+	// 	imageBlock.forEach((block) => {
+	// 		const imageSlider = block.querySelector('.imageswiper-js');
+
+	// 		const swiper = new Swiper(imageSlider, {
+	// 			direction: 'horizontal',
+	// 			slidesPerView: 1,
+	// 			spaceBetween: 20,
+	// 			navigation: {
+	// 				nextEl: imageSlider.querySelector('.swiper-buttons-next'),
+	// 				prevEl: imageSlider.querySelector('.swiper-buttons-prev'),
+	// 			}
+	// 		})
+	// 	})
+	// }
 }
 
 init();

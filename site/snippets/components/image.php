bbb549ca9bc55c0531dcdc337087bdee27d5b546
Rémy <lymih@hotmail.com>
2026-04-16 13:54:16 +0200

    remove useless image lines

--- site/snippets/components/image.php
+++ site/snippets/components/image.php
@@ -1,26 +1,17 @@
-<source 
-    alt="<?= (!$img->alt() ? $img->alt() : 'Website') ?>"
-    class="<?= (isset($class) ? $class : '') ?>"
+<source
     srcset="<?= $img->srcset('webp') ?>"
-    type="<?= $img->thumb(['format' => 'webp'])->mime(); ?>"
-    loading="lazy"
->
+    type="<?= $img->thumb(['format' => 'webp'])->mime(); ?>">
 
 
-<source 
-    alt="<?= (!$img->alt() ? $img->alt() : 'Website') ?>"
-    class="<?= (isset($class) ? $class : '') ?>"
+<source
     srcset="<?= $img->srcset() ?>"
-    type="<?= $img->thumb()->mime(); ?>"
-    loading="lazy"
->
+    type="<?= $img->thumb()->mime(); ?>">
 
 
-<img 
+<img
     alt="<?= (!$img->alt() ? $img->alt() : 'Website') ?>"
     class="<?= (isset($class) ? $class : '') ?>"
     srcset="<?= $img->srcset() ?>"
     src="<?= $img->url(); ?>"
     decoding="async"
-    loading="lazy"
-/>
+    loading="lazy" />

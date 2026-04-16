7b216544c541eb15ac081ed9626600ce37ff1e04
Rémy <lymih@hotmail.com>
2026-04-16 11:50:20 +0200

    switch to block based components

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,8 @@
 <?php snippet('header') ?>
 
-<?php $i = 0; foreach($page->children()->listed() as $block): ?>
-    <?php snippet('blocks/' . $block->intendedTemplate(), ['data' => $block]); ?>
+<?php $i = 0;
+foreach ($page->blocks1()->toBlocks() as $block): ?>
+    <?php snippet('blocks/' . $block->type(), ['data' => $block]); ?>
 <?php endforeach; ?>
 
 <?php snippet('footer') ?>
